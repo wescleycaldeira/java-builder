b7fae77fff0098bfad7236439476801cad762b70
Wescley <wescley13sri@gmail.com>
2021-04-15 10:11:14 -0300

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# java-builder
-Creational Builder Design Pattern
+# Builder
+Creational Design Pattern
+
+- Separate Component for Building an object
+- Builder a constructor or return it via a static function
+- To make fluent, return this
+- Different facets of an object can be built with different builders working in tandem via base class
+
+
+
--- src/com/wcaldeira/facets/Demo.java
+++ src/com/wcaldeira/facets/Demo.java
@@ -5,15 +5,15 @@ public class Demo {
     public static void main(String[] args) {
         PersonBuilder personBuilder = new PersonBuilder();
 
-        Person person = personBuilder
+        final Person person = personBuilder
                 .lives()
-                .at("SHCN 105 Asa Sul")
-                .in("Brasilia")
-                .withPostcode("11111-111")
+                    .at("SHCN 105 Asa Sul")
+                    .in("Brasilia")
+                    .withPostcode("11111-111")
                 .works()
-                .at("Marie")
-                .asA("Engineer")
-                .earning(250000)
+                    .at("Marie")
+                    .asA("Engineer")
+                    .earning(250000)
                 .build();
 
         System.out.println(person);
